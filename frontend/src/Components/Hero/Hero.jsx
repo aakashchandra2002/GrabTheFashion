9010f9dee7845adcc98537169427114d96ef919d
Aakash Chandra Vakkalagadda <aakashchandra2002@gmail.com>
2024-05-09 13:11:43 +0530

    Frontend Modified

---
+++ frontend/src/Components/Hero/Hero.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Hero.css'
+import hand_icon from '../Assets/hand_icon.png'
+import arrow_icon from '../Assets/arrow.png'
+import hero_img from '../Assets/hero.png'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className="hero-left">
+        <h2>NEW ARRIVALS ONLY</h2>
+        <div>
+            <div className="hero-hand-icon">
+                <p>new</p>
+                <img src={hand_icon}alt="" />
+            </div>
+            <p>collections</p>
+            <p>for everyone</p>
+        </div>
+        <div className="hero-latest-btn">
+            <div>Latest Collection</div>
+            <img src={arrow_icon} width={50}height={50} alt="" />
+        </div>
+        
+
+      </div>
+      <div className="hero-right">
+        <img src={hero_img}  width='1200px' height='800px' alt="" />
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Hero
